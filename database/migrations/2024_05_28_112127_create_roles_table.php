ac2337e24c0a476b8da934a24a89dfd4c8390423
kevokyforce <kevokyforce@gmail.com>
2024-05-28 15:17:05 +0300

    ERP System the Start using Vue js, Javascript and Laravel Jetstream

---
+++ database/migrations/2024_05_28_112127_create_roles_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
+        });
+
+        DB::table('roles')->insert([
+            [
+                'id' => 1,
+                'title' => 'Super Admin'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Admin'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Client'
+            ],
+            [
+                'id' => 4,
+                'title' => 'Supplier'
+            ],
+            [
+                'id' => 5,
+                'title' => 'Employee'
+            ],
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make(env('DEFAULT_PASSWORD')),
+            'role_id' => '1',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
