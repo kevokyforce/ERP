4ff86228f897184c548dacadeef583fc9361dcec
kevokyforce <kevokyforce@gmail.com>
2024-05-29 13:05:56 +0300

    Super Admin, Admin, Client, Employee, Supplier Middleware and Modules

---
+++ app/Http/Middleware/Client.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Client
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return auth()->user()->is_client ? $next($request) : abort(403, 'You are not authorized to this page contact Admin for more');
+    }
+}
---
+++ app/Http/Middleware/SuperAdmin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SuperAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return auth()->user()->is_super ? $next($request) : abort(403, 'You are not a Superior Administrator!');
+    }
+}
